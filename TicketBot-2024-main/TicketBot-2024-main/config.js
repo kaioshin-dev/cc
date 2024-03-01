fe1f311d237bba760ae68a5039539202536674ec
kaioshin-dev <159612562+kaioshin-dev@users.noreply.github.com>
2024-03-01 10:19:42 +0700

    Add files via upload

---
+++ TicketBot-2024-main/TicketBot-2024-main/config.js
@@ -0,0 +1,40 @@
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
+
+module.exports = {
+  TOKEN: "MTE4MzM2NDgyMTM0NTkxNDk0MA.Ghai4n.0IIo2IbuWeitRNO9wX7DAumq4xyZYST7014-Pk",
+  ownerID: ["1004206704994566164", "779507251282968587"], 
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "00fbff",
+  errorLog: "", 
+
+}
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
